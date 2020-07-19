097a233697bcc0cf4a06cdce0d142d9d3da1ee61
NadimArubai <nadim.arubai@gmail.com>
2020-07-19 15:19:48 +0300

    add semseg_for_onnx to init.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,6 +3,7 @@ import torch
 import os
 import tqdm 
 from . import semseg
+from . import semseg_for_onnx
 import torch
 
 
@@ -16,7 +17,15 @@ def get_model(model_dict, exp_dict=None, train_set=None):
  
     return model
 
+def get_model_for_onnx(model_dict, exp_dict=None, train_set=None):
+    if model_dict['name'] in ["semseg"]:
+        model =  semseg_for_onnx.SemSeg(exp_dict, train_set)
 
+        # load pretrained
+        if 'pretrained' in model_dict:
+            model.load_state_dict(torch.load(model_dict['pretrained']))
+ 
+    return model
 
 
 
