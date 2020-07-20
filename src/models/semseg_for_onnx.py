5bf4a827cd2e03797a0f9f1834fc00cfa54f97cb
NadimArubai <nadim.arubai@gmail.com>
2020-07-20 11:01:52 +0300

    fix semseg for onnx

--- src/models/semseg_for_onnx.py
+++ src/models/semseg_for_onnx.py
@@ -45,6 +45,8 @@ class SemSeg(torch.nn.Module):
         else:
             raise ValueError
 
+        self.just_one = True
+
     def get_state_dict(self):
         state_dict = {"model": self.model_base.state_dict(),
                       "opt": self.opt.state_dict()}
@@ -57,25 +59,25 @@ class SemSeg(torch.nn.Module):
             return
         self.opt.load_state_dict(state_dict["opt"])
 
-    def train_on_loader(self, train_loader):
-        self.train()
+    # def train_on_loader(self, train_loader):
+    #     self.train()
 
-        n_batches = len(train_loader)
+    #     n_batches = len(train_loader)
 
-        pbar = tqdm.tqdm(desc="Training", total=n_batches, leave=False)
-        train_monitor = TrainMonitor()
+    #     pbar = tqdm.tqdm(desc="Training", total=n_batches, leave=False)
+    #     train_monitor = TrainMonitor()
     
-        for batch in train_loader:
-            score_dict = self.train_on_batch(batch)
+    #     for batch in train_loader:
+    #         score_dict = self.train_on_batch(batch)
             
-            train_monitor.add(score_dict)
-            msg = ' '.join(["%s: %.3f" % (k, v) for k,v in train_monitor.get_avg_score().items()])
-            pbar.set_description('Training - %s' % msg)
-            pbar.update(1)
+    #         train_monitor.add(score_dict)
+    #         msg = ' '.join(["%s: %.3f" % (k, v) for k,v in train_monitor.get_avg_score().items()])
+    #         pbar.set_description('Training - %s' % msg)
+    #         pbar.update(1)
             
-        pbar.close()
+    #     pbar.close()
 
-        return train_monitor.get_avg_score()
+    #     return train_monitor.get_avg_score()
 
 
     @torch.no_grad()
@@ -84,25 +86,38 @@ class SemSeg(torch.nn.Module):
 
         # metrics
         seg_monitor = metrics.SegMonitor()
-
         n_batches = len(val_loader)
-        pbar = tqdm.tqdm(desc="Validating", total=n_batches, leave=False)
+        # pbar = tqdm.tqdm(desc="Validating", total=n_batches, leave=False)
         for i, batch in enumerate(val_loader):
             seg_monitor.val_on_batch(self, batch)
-              
-            pbar.update(1)
+            if self.just_one:
+                net_name = self.exp_dict['model'].get('base', 'unet2d')
+                print(net_name)
+                if net_name == 'unet2d':
+                    torch.onnx._export(self.model_base, batch['images'].cuda(), 'onnx_model_unet2d.onnx', verbose=False, opset_version=12)
+                    print('Exported to onnx_model_unet2d.onnx')
+                elif net_name == 'pspnet':
+                    torch.onnx._export(self.model_base, batch['images'].cuda(), 'onnx_model_pspnet.onnx', verbose=False, opset_version=12, operator_export_type=torch.onnx.OperatorExportTypes.ONNX_ATEN_FALLBACK)
+                    print('Exported to onnx_model_pspnet.onnx')
+                else:
+                    print('not supported network to export.')
+                self.just_one = False
 
-            if savedir_images and i < n_images:
-                os.makedirs(savedir_images, exist_ok=True)
-                self.vis_on_batch(batch, savedir_image=os.path.join(
-                    savedir_images, "%d.jpg" % i), save_preds=save_preds)
-                pbar.set_description("Validating & Saving Images: %.4f mIoU" %
-                                 (seg_monitor.get_avg_score()['val_score']))
-            else:
-                pbar.set_description("Validating: %.4f mIoU" %
-                                 (seg_monitor.get_avg_score()['val_score']))
+            # pbar.update(1)
 
-        pbar.close()
+            # if savedir_images and i < n_images:
+            #     os.makedirs(savedir_images, exist_ok=True)
+                #self.vis_on_batch(batch, savedir_image=os.path.join(
+                #    savedir_images, "%d.jpg" % i), save_preds=save_preds)
+                # pbar.set_description("Validating & Saving Images: %.4f mIoU" %
+                                #  (seg_monitor.get_avg_score()['val_score']))
+            # else:
+                # pbar.set_description("Validating: %.4f mIoU" %
+                                #  (seg_monitor.get_avg_score()['val_score']))
+# remove break to scan all images and get the full score
+            break
+
+        # pbar.close()
         val_dict = seg_monitor.get_avg_score()
         out_dict = {}
         for c in range(self.n_classes):
@@ -110,27 +125,27 @@ class SemSeg(torch.nn.Module):
         out_dict['val_score'] = val_dict['val_score']
         return out_dict
 
-    def train_on_batch(self, batch, **extras):
-        self.train()
-        self.model_base.train()
-        self.opt.zero_grad()
+    # def train_on_batch(self, batch, **extras):
+    #     self.train()
+    #     self.model_base.train()
+    #     self.opt.zero_grad()
 
-        images, labels = batch["images"], batch["masks"]
-        images, labels = images.cuda(), labels.cuda()
+    #     images, labels = batch["images"], batch["masks"]
+    #     images, labels = images.cuda(), labels.cuda()
         
-        logits = self.model_base(images)
-        # match image size
-        logits = match_image_size(images, logits)
-        # compute loss
-        loss_name = self.exp_dict['model'].get('loss', 'cross_entropy')
-        loss = losses.compute_loss(loss_name, logits, labels)
+    #     logits = self.model_base(images)
+    #     # match image size
+    #     logits = match_image_size(images, logits)
+    #     # compute loss
+    #     loss_name = self.exp_dict['model'].get('loss', 'cross_entropy')
+    #     loss = losses.compute_loss(loss_name, logits, labels)
         
-        if loss != 0:
-            loss.backward()
+    #     if loss != 0:
+    #         loss.backward()
 
-        self.opt.step()
+    #     self.opt.step()
 
-        return {"train_loss": float(loss)}
+    #     return {"train_loss": float(loss)}
 
     def predict_on_batch(self, batch):
         images = batch["images"].cuda()
@@ -140,100 +155,102 @@ class SemSeg(torch.nn.Module):
         logits = match_image_size(images, logits)
         return logits.argmax(dim=1)
 
-    @torch.no_grad()
-    def vis_on_batch(self, batch, savedir_image, save_preds=False):
-        # os.makedirs(savedir_image, exist_ok=True)
-        self.eval()
-        
-        onnx_output = torch.onnx._export(self.model_base, torch_input.cuda(), 'onnx_model.onnx', verbose=False, opset_version=11)
+    # @torch.no_grad()
+    # def vis_on_batch(self, batch, savedir_image, save_preds=False):
+    #     # os.makedirs(savedir_image, exist_ok=True)
+    #     #self.eval()
+
+    #     if self.just_one:
+    #       onnx_output = torch.onnx._export(self.model_base, batch['images'].cuda(), 'onnx_model.onnx', verbose=False, opset_version=12)
+    #       self.just_one = False
 
         #onnx_output = torch.onnx._export(self.model_base, torch_input.cuda(), 'onnx_model2.onnx', verbose=False, opset_version=11, operator_export_type=torch.onnx.OperatorExportTypes.ONNX_ATEN_FALLBACK)
 
 
-        # clf
-        pred_mask = self.predict_on_batch(batch).cpu()
-        # print(pred_mask.sum())
-        img = hu.f2l(batch['images'])[0]
-        img += abs(img.min())
-        img /= img.max()
-        img = img.repeat(1,1,3)
+        # # clf
+        # pred_mask = self.predict_on_batch(batch).cpu()
+        # # print(pred_mask.sum())
+        # img = hu.f2l(batch['images'])[0]
+        # img += abs(img.min())
+        # img /= img.max()
+        # img = img.repeat(1,1,3)
 
-        mask_vis = batch["masks"].clone().float()[0][..., None]
-        mask_vis[mask_vis == 255] = 0
+        # mask_vis = batch["masks"].clone().float()[0][..., None]
+        # mask_vis[mask_vis == 255] = 0
 
-        pred_mask_vis = pred_mask.clone().float()[0][..., None]
-        vmax = 0.1
+        # pred_mask_vis = pred_mask.clone().float()[0][..., None]
+        # vmax = 0.1
 
-        fig, ax_list = plt.subplots(ncols=3, nrows=1)
-        ax_list[0].imshow(img[:, :, 0], cmap='gray',
-                        #   interpolation='sinc', vmin=0, vmax=0.4
-                          )
+        # fig, ax_list = plt.subplots(ncols=3, nrows=1)
+        # ax_list[0].imshow(img[:, :, 0], cmap='gray',
+        #                 #   interpolation='sinc', vmin=0, vmax=0.4
+        #                   )
 
-        colors_all = np.array(['black', 'red', 'blue', 'green', 'purple'])
-        colors = colors_all[np.unique(mask_vis).astype(int)]
+        # colors_all = np.array(['black', 'red', 'blue', 'green', 'purple'])
+        # colors = colors_all[np.unique(mask_vis).astype(int)]
 
-        vis = label2rgb(mask_vis[:, :, 0].numpy(), image=img.numpy(
-        ), colors=colors, bg_label=255, bg_color=None, alpha=0.6, kind='overlay')
-        vis = mark_boundaries(
-            vis, mask_vis[:, :, 0].numpy().astype('uint8'), color=(1, 1, 1))
+        # vis = label2rgb(mask_vis[:, :, 0].numpy(), image=img.numpy(
+        # ), colors=colors, bg_label=255, bg_color=None, alpha=0.6, kind='overlay')
+        # vis = mark_boundaries(
+        #     vis, mask_vis[:, :, 0].numpy().astype('uint8'), color=(1, 1, 1))
 
-        ax_list[1].imshow(vis, cmap='gray')
+        # ax_list[1].imshow(vis, cmap='gray')
 
-        colors = colors_all[np.unique(pred_mask_vis).astype(int)]
-        vis = label2rgb(pred_mask_vis[:, :, 0].numpy(), image=img.numpy(
-        ), colors=colors, bg_label=255, bg_color=None, alpha=0.6, kind='overlay')
-        vis = mark_boundaries(
-            vis, pred_mask_vis[:, :, 0].numpy().astype('uint8'), color=(1, 1, 1))
+        # colors = colors_all[np.unique(pred_mask_vis).astype(int)]
+        # vis = label2rgb(pred_mask_vis[:, :, 0].numpy(), image=img.numpy(
+        # ), colors=colors, bg_label=255, bg_color=None, alpha=0.6, kind='overlay')
+        # vis = mark_boundaries(
+        #     vis, pred_mask_vis[:, :, 0].numpy().astype('uint8'), color=(1, 1, 1))
 
-        ax_list[2].imshow(vis, cmap='gray')
+        # ax_list[2].imshow(vis, cmap='gray')
 
-        for i in range(1, self.n_classes):
-            plt.plot([None], [None], label='group %d' % i, color=colors_all[i])
-        # ax_list[1].axis('off')
-        ax_list[0].grid()
-        ax_list[1].grid()
-        ax_list[2].grid()
+        # for i in range(1, self.n_classes):
+        #     plt.plot([None], [None], label='group %d' % i, color=colors_all[i])
+        # # ax_list[1].axis('off')
+        # ax_list[0].grid()
+        # ax_list[1].grid()
+        # ax_list[2].grid()
 
-        ax_list[0].tick_params(axis='x', labelsize=6)
-        ax_list[0].tick_params(axis='y', labelsize=6)
+        # ax_list[0].tick_params(axis='x', labelsize=6)
+        # ax_list[0].tick_params(axis='y', labelsize=6)
 
-        ax_list[1].tick_params(axis='x', labelsize=6)
-        ax_list[1].tick_params(axis='y', labelsize=6)
+        # ax_list[1].tick_params(axis='x', labelsize=6)
+        # ax_list[1].tick_params(axis='y', labelsize=6)
 
-        ax_list[2].tick_params(axis='x', labelsize=6)
-        ax_list[2].tick_params(axis='y', labelsize=6)
+        # ax_list[2].tick_params(axis='x', labelsize=6)
+        # ax_list[2].tick_params(axis='y', labelsize=6)
 
-        ax_list[0].set_title('Original image', fontsize=8)
-        ax_list[1].set_title('Ground-truth',  fontsize=8)
-        ax_list[2].set_title('Prediction',  fontsize=8)
+        # ax_list[0].set_title('Original image', fontsize=8)
+        # ax_list[1].set_title('Ground-truth',  fontsize=8)
+        # ax_list[2].set_title('Prediction',  fontsize=8)
 
-        legend_kwargs = {"loc": 2, "bbox_to_anchor": (1.05, 1),
-                         'borderaxespad': 0., "ncol": 1}
-        ax_list[2].legend(fontsize=6, **legend_kwargs)
-        plt.savefig(savedir_image.replace('.jpg', '.png'),
-                    bbox_inches='tight', dpi=300)
-        plt.close()
+        # legend_kwargs = {"loc": 2, "bbox_to_anchor": (1.05, 1),
+        #                  'borderaxespad': 0., "ncol": 1}
+        # ax_list[2].legend(fontsize=6, **legend_kwargs)
+        # plt.savefig(savedir_image.replace('.jpg', '.png'),
+        #             bbox_inches='tight', dpi=300)
+        # plt.close()
 
-        # save predictions
-        if save_preds:
-            from PIL import Image
-            pred_dict = {}
-            pred_numpy = pred_mask.cpu().numpy().squeeze().astype('uint8')
+        # # save predictions
+        # if save_preds:
+        #     from PIL import Image
+        #     pred_dict = {}
+        #     pred_numpy = pred_mask.cpu().numpy().squeeze().astype('uint8')
 
-            uniques = np.unique(np.array(pred_numpy))
-            # print(uniques)
-            meta_dict = batch['meta'][0]
+        #     uniques = np.unique(np.array(pred_numpy))
+        #     # print(uniques)
+        #     meta_dict = batch['meta'][0]
 
-            for u in range(self.n_classes):
-                meta_dict['gt_group%d_n_pixels'%u] = float((batch['masks']==u).float().sum())
-                meta_dict['pred_group%d_n_pixels'%u] = float((pred_mask==u).float().sum())
+        #     for u in range(self.n_classes):
+        #         meta_dict['gt_group%d_n_pixels'%u] = float((batch['masks']==u).float().sum())
+        #         meta_dict['pred_group%d_n_pixels'%u] = float((pred_mask==u).float().sum())
                 
-                if u == 0:
-                    continue
-                pred = Image.fromarray(pred_numpy==u)
-                pred.save(savedir_image.replace('.jpg', '_group%d.png'%u))
+        #         if u == 0:
+        #             continue
+        #         pred = Image.fromarray(pred_numpy==u)
+        #         pred.save(savedir_image.replace('.jpg', '_group%d.png'%u))
 
-            hu.save_json(savedir_image.replace('.jpg', '.json'), meta_dict)
+        #     hu.save_json(savedir_image.replace('.jpg', '.json'), meta_dict)
             
 
 def match_image_size(images, logits):
@@ -243,18 +260,19 @@ def match_image_size(images, logits):
         logits = F.interpolate(logits, (h,w), mode='bilinear', align_corners=True)
     return logits
 
-class TrainMonitor:
-    def __init__(self):
-        self.score_dict_sum = {}
-        self.n = 0
+# class TrainMonitor:
+#     def __init__(self):
+#         self.score_dict_sum = {}
+#         self.n = 0
 
-    def add(self, score_dict):
-        for k,v in score_dict.items():
-            if k not in self.score_dict_sum:
-                self.score_dict_sum[k] = score_dict[k]
-            else:
-                self.n += 1
-                self.score_dict_sum[k] += score_dict[k]
+#     def add(self, score_dict):
+#         for k,v in score_dict.items():
+#             if k not in self.score_dict_sum:
+#                 self.score_dict_sum[k] = score_dict[k]
+#             else:
+#                 self.n += 1
+#                 self.score_dict_sum[k] += score_dict[k]
 
-    def get_avg_score(self):
-        return {k:v/(self.n + 1) for k,v in self.score_dict_sum.items()}
+#     def get_avg_score(self):
+#         return {k:v/(self.n + 1) for k,v in self.score_dict_sum.items()}
+
